a1f22ab0b71c0f7dc2f5e4243035dd4b0b83cc76
DeronLee <akagilnc@gmail.com>
2019-02-23 13:24:15 +0900

    data binding break

--- js/index.js
+++
@@ -1,8 +0,0 @@
-import View from 'dom-renderer';
-
-import template from './index.html';
-import data from './index.json';
-
-const view = new View( template );
-
-view.render( data ).then(() => console.log(view + ''));
